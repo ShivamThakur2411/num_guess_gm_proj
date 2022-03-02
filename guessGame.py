5c2a3ed528a6b5dbb4d288c7f0b1f19a42921522
ShivamThakur2411 <65968482+ShivamThakur2411@users.noreply.github.com>
2022-03-02 21:20:53 +0530

    Add files via upload

---
+++ guessGame.py
@@ -0,0 +1,21 @@
+import random
+number = random.randrange(0,9)
+
+print("================NUMBER GUESSING GAME================")
+guess = int(input("ENTER THE NUMBER YOU THINK OF - "))
+attemptCount=0
+
+while attemptCount<5:
+	attemptCount = attemptCount + 1
+	if guess>number:
+		print("The number you guessed was higher than the correct number")
+		guess = int(input("ENTER THE NUMBER YOU THINK OF - "))
+	elif guess<number:
+		print("The number you guessed was lower than the correct number")
+		guess = int(input("ENTER THE NUMBER YOU THINK OF - "))
+	elif guess==number:
+		print("CONGRATULATIONS, YOU WON!!")
+		break
+	if attemptCount==5 & guess!=number:
+		print("Sorry, you lost the game, the correct number was - ")
+		print(number)
